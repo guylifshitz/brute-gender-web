d58f439c7cff996b2de8ecefcc9df68b8f5434dc
Guy Lifshitz <guylifshitz@gmail.com>
2016-09-21 16:05:46 -0400

    change to mysql

--- db/schema.rb
+++ db/schema.rb
@@ -14,39 +14,39 @@
 ActiveRecord::Schema.define(version: 20160921010802) do
 
   create_table "levels", force: :cascade do |t|
-    t.string   "name"
-    t.string   "description"
-    t.string   "words"
-    t.string   "statistics"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "name",        limit: 255
+    t.string   "description", limit: 255
+    t.string   "words",       limit: 255
+    t.string   "statistics",  limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "words", force: :cascade do |t|
-    t.string   "word"
-    t.string   "definition_en"
-    t.string   "definition_fr"
-    t.integer  "gender"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.string   "word",          limit: 255
+    t.string   "definition_en", limit: 255
+    t.string   "definition_fr", limit: 255
+    t.integer  "gender",        limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
 end
