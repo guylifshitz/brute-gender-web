a386768085b582703d66b8ba7ab0579e518a36fc
Guy Lifshitz <guylifshitz@gmail.com>
2016-10-22 02:34:01 +0200

    Trying to fix capistrano issues

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,43 +59,9 @@ set :pty,  false
 # sidekiq_cmd_pre = 'cd #{current_path} && ( PATH=/home/guy/.rvm/bin:$PATH RAILS_ENV=production ~/.rvm/bin/rvm default do'
 # set :sidekiq_cmd, -> { "#{sidekiq_cmd_pre} bundle exec sidekiq)" }
 
-
 namespace :sidekiq do
-  def sidekiq_pid
-    current_path + '../shared/pids/sidekiq.pid'
-  end
-
-  def pid_file_exists?
-    test(*("[ -f #{sidekiq_pid} ]").split(' '))
-  end
-
-  def pid_process_exists?
-    pid_file_exists? and test(*("kill -0 $( cat #{sidekiq_pid} )").split(' '))
-  end
-
   task :start do
-    on roles(:app) do
-      if !pid_process_exists?
-        execute "cd #{current_path} && RAILS_ENV=#{fetch(:rails_env)} #{fetch(:rbenv_prefix)} bundle exec sidekiq -C config/sidekiq.yml -e #{fetch(:rails_env)} -L log/sidekiq.log -P #{sidekiq_pid} -d"
-      end
-    end
-  end
-
-  task :stop do
-    on roles(:app) do
-      if pid_process_exists?
-        execute "kill `cat #{sidekiq_pid}`"
-        execute "rm #{sidekiq_pid}"
-      end
-    end
-  end
-
-  task :restart do
-    on roles(:app) do
-      invoke "sidekiq:stop"
-      invoke "sidekiq:start"
-    end
+     execute "/home/guy/.rvm/gems/ruby-2.3.0/bin/bundle exec sidekiq --index 0 --pidfile /home/guy/brute-gender-web/shared/tmp/pids/sidekiq.pid --environment production"
   end
 end
 
-after 'deploy:restart', 'sidekiq:start'
